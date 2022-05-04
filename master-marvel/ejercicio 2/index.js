50d52b25931a2c33444992643c3deb622c3c90cb
jaredvel <jaredvelazquez12@gmail.com>
2022-05-03 21:31:53 -0500

    ejercicios

--- master-marvel/ejercicio 2/index.js
+++ master-marvel/ejercicio 2/index.js
@@ -22,10 +22,17 @@ const matrizResultado2 = [
     [0,0,0],
 ];
 
+const matrizResultado3 = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0],
+];
+
     []
 
 console.log("matrizA =>", matrizA);
 console.log("matrizB =>", matrizB);
+
 
 for (var fila = 0; fila < matrizResultado.length; fila++) {
     for (var columna = 0; columna < matrizResultado.length; columna++) {
@@ -41,5 +48,39 @@ for (var fila = 0; fila < 3; fila++) {
     }
 }
 
-console.log("matrizResultado =>", matrizResultado);
-console.log("matrizResultado2 =>", matrizResultado2);
+const matrizA2 = [
+    [1,6,0],
+    [-1,3,1],
+    [4,6,2],
+];
+
+const matrizB2 = [
+    [0,4,0],
+    [2,3,1],
+    [1,-2,1],
+];
+
+
+for (var fila = 0; fila < matrizResultado3.length; fila++) {
+    for (var columna = 0; columna < matrizResultado3.length; columna++) {
+        //console.log(matrizB[fila][columna]);
+        //matrizResultado3[fila][columna] = matrizResultado3[fila][columna] + matrizA[fila][columna] + matrizB[fila][columna]
+
+        for (var aux = 0; aux < matrizResultado3.length; aux++) {
+            matrizResultado3[fila][columna] = matrizResultado3[fila][columna] + matrizA[fila][aux] + matrizB[aux][columna]
+        }
+
+        //matrizResultado3[0][0] = matrizResultado3[0][0] + matrizA[0][0] + matrizB[0][0]
+        //matrizResultado3[0][0] = matrizResultado3[0][0] + matrizA[0][1] + matrizB[1][0]
+        //matrizResultado3[0][0] = matrizResultado3[0][0] + matrizA[0][2] + matrizB[2][0]
+
+    }
+}
+
+console.log("matrizResultado (suma) =>", matrizResultado);
+console.log("matrizResultado2 (resta) =>", matrizResultado2);
+
+console.log("matrizA =>", matrizA2);
+console.log("matrizB =>", matrizB2);
+
+console.log("matrizResultado3 (multi) =>", matrizResultado3);
